57bd2d493b583ebca7473bf0119bc239c0b7b7b3
Brian Ashman <77475043+brianashman@users.noreply.github.com>
2026-07-20 01:46:47 -0400

    Fixed app.axaml.cs to implement Dependency Injection Pattern

--- WyrdStack.Maui/App.xaml.cs
+++ WyrdStack.Maui/App.xaml.cs
@@ -5,20 +5,23 @@ namespace WyrdStack.Maui
 {
 	public partial class App : Application
 	{
-		public App()
+		private readonly IServiceProvider _serviceProvider;
+		public App(IServiceProvider serviceProvider)
 		{
 			InitializeComponent();
+			_serviceProvider = serviceProvider;
 		}
 		protected override Window CreateWindow(IActivationState? activationState)
 		{
 			var token = SecureStorage.Default.GetAsync("auth_token").Result;
 			if(token == null)
 			{
-				return new Window(new LoginPage());
+
+				return new Window(_serviceProvider.GetRequiredService<LoginPage>());
 			}
 			else
 			{
-				return new Window(new AppShell());
+				return new Window(_serviceProvider.GetRequiredService<AppShell>());
 			}
 		}
 	}
