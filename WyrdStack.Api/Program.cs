26ac2ad1454af7bdc5120b3ba44ee7b06e7e05c7
Brian Ashman <77475043+brianashman@users.noreply.github.com>
2026-07-20 02:48:23 -0400

    Setup Api password auth rules

--- WyrdStack.Api/Program.cs
+++ WyrdStack.Api/Program.cs
@@ -12,8 +12,17 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<DataContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddAuthorization();
-builder.Services.AddIdentityApiEndpoints<IdentityUser>()
-	.AddEntityFrameworkStores<DataContext>();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>(options =>
+{
+	options.Password.RequiredLength = 8;
+	options.Password.RequireNonAlphanumeric = false;
+	options.Password.RequireLowercase = true;
+	options.Password.RequireUppercase = true;
+	options.Password.RequireDigit = true;
+	
+	options.User.RequireUniqueEmail = false;
+}
+).AddEntityFrameworkStores<DataContext>();
 
 var app = builder.Build();
 
