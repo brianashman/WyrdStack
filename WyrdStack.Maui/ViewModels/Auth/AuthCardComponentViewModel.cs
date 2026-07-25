1767c2b8652b944d7000a987ff6643aa8b7534c2
Brian Ashman <77475043+brianashman@users.noreply.github.com>
2026-07-24 20:11:17 -0400

    Updated Project Structure

--- WyrdStack.Maui/ViewModels/Auth/AuthCardComponentViewModel.cs
+++ WyrdStack.Maui/ViewModels/Auth/AuthCardComponentViewModel.cs
@@ -1,54 +1,54 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿	using CommunityToolkit.Mvvm.ComponentModel;
+	using CommunityToolkit.Mvvm.Input;
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
 
-namespace WyrdStack.Maui.ViewModels.Auth
-{
-	//BaseModel for the AuthCardComponent, which is used in the LoginPage and RegisterPage
-	public abstract partial class AuthCardComponentViewModel: BaseViewModel
+	namespace WyrdStack.Maui.ViewModels.Auth
 	{
-		[ObservableProperty] public partial string Title { get; set; }
-		
-		[ObservableProperty] public partial string Email { get; set; }
-		partial void OnEmailChanged(string oldValue, string newValue)
+		//BaseModel for the AuthCardComponent, which is used in the LoginPage and RegisterPage
+		public abstract partial class AuthCardComponentViewModel: BaseViewModel
 		{
-			if(newValue?.Contains(" ") is true)
+			[ObservableProperty] public partial string Title { get; set; }
+			
+			[ObservableProperty] public partial string Email { get; set; }
+			partial void OnEmailChanged(string oldValue, string newValue)
 			{
-				Email = newValue.Replace(" ", "");
+				if(newValue?.Contains(" ") is true)
+				{
+					Email = newValue.Replace(" ", "");
+				}
 			}
-		}
-		[ObservableProperty] public partial string Username { get; set; }
-		partial void OnUsernameChanged(string oldValue, string newValue)
-		{
-			if(newValue?.Contains(" ") is true)
+			[ObservableProperty] public partial string Username { get; set; }
+			partial void OnUsernameChanged(string oldValue, string newValue)
 			{
-				Username = newValue.Replace(" ", "");
+				if(newValue?.Contains(" ") is true)
+				{
+					Username = newValue.Replace(" ", "");
+				}
 			}
-		}
 
-		[ObservableProperty] public partial string Password { get; set; }
-		partial void OnPasswordChanged(string oldValue, string newValue)
-		{
-			if(newValue?.Contains(" ") is true)
+			[ObservableProperty] public partial string Password { get; set; }
+			partial void OnPasswordChanged(string oldValue, string newValue)
 			{
-				Password = newValue.Replace(" ", "");
+				if(newValue?.Contains(" ") is true)
+				{
+					Password = newValue.Replace(" ", "");
+				}
 			}
+
+			[ObservableProperty] public partial bool IsPassword { get; set; }
+
+			[ObservableProperty] public partial string ActionButtonText { get; set; }
+			[ObservableProperty] public partial bool HasUsernameEntry { get; set; }
+			[ObservableProperty] public partial string StatusMessage { get; set; }
+			[ObservableProperty] public partial bool IsLoading { get; set; }
+
+			[RelayCommand]
+			public void TogglePasswordVisibility() => IsPassword = !IsPassword;
+
+			[RelayCommand] protected virtual async void ExecuteActionButton() { }
+
+			[RelayCommand] protected virtual async Task NavigateToAsync() { }
 		}
-
-		[ObservableProperty] public partial bool IsPassword { get; set; }
-
-		[ObservableProperty] public partial string ActionButtonText { get; set; }
-		[ObservableProperty] public partial bool HasUsernameEntry { get; set; }
-		[ObservableProperty] public partial string StatusMessage { get; set; }
-		[ObservableProperty] public partial bool IsLoading { get; set; }
-
-		[RelayCommand]
-		public void TogglePasswordVisibility() => IsPassword = !IsPassword;
-
-		[RelayCommand] protected virtual async void ExecuteActionButton() { }
-
-		[RelayCommand] protected virtual async Task NavigateToAsync() { }
 	}
-}
