d4920249f96f5427040f1edf8e49161222b773a4
Brian Ashman <77475043+brianashman@users.noreply.github.com>
2026-07-24 22:29:37 -0400

    Implemented Logout functionality

--- WyrdStack.Maui/ViewModels/MainPageViewModel.cs
+++ WyrdStack.Maui/ViewModels/MainPageViewModel.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using WyrdStack.Maui.Services.Api;
+using WyrdStack.Maui.Services.Navigation;
 
 namespace WyrdStack.Maui.ViewModels
 {
 	public partial class MainPageViewModel : BaseViewModel
 	{
+		private readonly INavigationService _navigationService;
+		public MainPageViewModel(INavigationService navigationService)
+		{
+			_navigationService = navigationService;
+		}
+
+		[RelayCommand]
+		private void Logout()
+		{
+			SecureStorage.Default.Remove("auth_token");
+			_navigationService.GoToAbsoluteAsync("LoginPage");
+		}
 	}
 }
